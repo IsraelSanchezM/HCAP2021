5814f7e919b0f81ad5aa6384ebf5d91380e81340
IsraelSanchezM <A01378705@itesm.mx>
2021-03-19 07:48:36 -0600

    Cambio en main

--- cargaImagen.py
+++ cargaImagen.py
@@ -5,3 +5,7 @@ import numpy as np
 IRGB = cv2.imread('004.jpg')
 print(IRGB)
 print(IRGB.shape)
+
+#Imprimiendo el tamaño de la imagen como matriz
+print('Modificación en la rama main')
+print(len(IRGB))
